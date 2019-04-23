abe158e7e4457e9b070efa540b7fedec2de559cd
Anila <anilanarun@gmail.com>
2019-04-23 01:40:33 +0100

    corrected the WebCrawlerControllerTest

--- apiproject/src/test/java/com/webcrawler/apiproject/controller/WebCrawlerControllerTest.java
+++ apiproject/src/test/java/com/webcrawler/apiproject/controller/WebCrawlerControllerTest.java
@@ -1,47 +1,27 @@
 package com.webcrawler.apiproject.controller;
 
-import com.webcrawler.apiproject.domain.CustomerProfile;
-import com.webcrawler.apiproject.domain.FlightInformation;
 import com.webcrawler.apiproject.domain.FormSubmission;
-import com.webcrawler.apiproject.domain.TravelInformation;
 import com.webcrawler.apiproject.service.CustomerProfileService;
 import com.webcrawler.apiproject.service.FlightInformationService;
 import com.webcrawler.apiproject.service.TravelInformationService;
-import org.easymock.EasyMock;
-import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockserver.integration.ClientAndServer;
-import org.mockserver.model.HttpRequest;
-import org.mockserver.model.HttpResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 
 import java.time.LocalDate;
-import java.text.SimpleDateFormat;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
-import static org.mockserver.integration.ClientAndServer.startClientAndServer;
 
 @RunWith(SpringRunner.class)
 public class WebCrawlerControllerTest {
-
-    private static ClientAndServer mockServer;
     WebCrawlerController webCrawlerController;
-
-    @Mock
-    CustomerProfile customerProfile;
     LocalDate date;
     @Mock
     BindingResult bindingResult;
@@ -53,19 +33,15 @@ public class WebCrawlerControllerTest {
     TravelInformationService travelInformationService;
     @Mock
     FlightInformationService flightInformationService;
-    private MockMvc mockMvc;
-    @InjectMocks
-    WebCrawlerController controller;
-    String url = "http://localhost:8082/h2";
 
     @Before
     public void setup() throws Exception {
         MockitoAnnotations.initMocks(this);
         webCrawlerController = new WebCrawlerController();
         date = LocalDate.of(2019, 07, 17);
-        mockServer = startClientAndServer(8082);
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-        customerProfileService = EasyMock.createMock(CustomerProfileService.class);
+        webCrawlerController.setCustomerProfileService(customerProfileService);
+        webCrawlerController.setFlightInformationService(flightInformationService);
+        webCrawlerController.setTravelInformationService(travelInformationService);
     }
 
     @Test
@@ -79,8 +55,6 @@ public class WebCrawlerControllerTest {
 
     @Test
     public void callSaveCustomerData() throws Exception {
-        mockServer.when(HttpRequest.request()).respond(HttpResponse.response().withBody("success"));
-        CustomerProfile customerProfile = new CustomerProfile();
         FormSubmission formSubmission = new FormSubmission();
         formSubmission.setCustomerEmail("test@test.com");
         formSubmission.setFirstName("FirstName");
@@ -93,10 +67,6 @@ public class WebCrawlerControllerTest {
 
         webCrawlerController.saveCustomerData(formSubmission, bindingResult, model);
         verify(customerProfileService, times(1)).save(any());
-        verify(travelInformationService, times(1)).save(any());
-    }
-    @After
-    public void stop() {
-        mockServer.stop();
+        verify(travelInformationService, times(1)).updateWithCustomerAndFlightData(any(), any());
     }
 }
